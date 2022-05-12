24c5c5975102aeea46abf8e877d6c5464c00b4fe
Franco <francobanfi01@gmail.com>
2022-05-12 13:10:22 -0300

    Mejorado de nombre de tests y tests funcionando

--- src/test/java/dds/monedero/model/MonederoTest.java
+++ src/test/java/dds/monedero/model/MonederoTest.java
@@ -4,6 +4,7 @@ import dds.monedero.exceptions.MaximaCantidadDepositosException;
 import dds.monedero.exceptions.MaximoExtraccionDiarioException;
 import dds.monedero.exceptions.MontoNegativoException;
 import dds.monedero.exceptions.SaldoMenorException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -19,23 +20,24 @@ public class MonederoTest {
 
   @Test
   void Poner() {
-    cuenta.poner(1500);
+    Assertions.assertDoesNotThrow(() -> cuenta.poner(1500));
   }
 
   @Test
-  void PonerMontoNegativo() {
+  void PonerMontoNegativoTiraUnaExcepcion() {
     assertThrows(MontoNegativoException.class, () -> cuenta.poner(-1500));
   }
 
   @Test
-  void TresDepositos() {
+  void TresDepositosConMontosCorrectosSeSumanAlSaldoDeLaCuenta() {
     cuenta.poner(1500);
     cuenta.poner(456);
     cuenta.poner(1900);
+    Assertions.assertEquals(cuenta.getSaldo(), 3856);
   }
 
   @Test
-  void MasDeTresDepositos() {
+  void HacerMasDeTresDepositosEnUnDiaTiraExcepcion() {
     assertThrows(MaximaCantidadDepositosException.class, () -> {
           cuenta.poner(1500);
           cuenta.poner(456);
@@ -45,7 +47,7 @@ public class MonederoTest {
   }
 
   @Test
-  void ExtraerMasQueElSaldo() {
+  void ExtraerMasQueElSaldoTiraExcepcion() {
     assertThrows(SaldoMenorException.class, () -> {
           cuenta.setSaldo(90);
           cuenta.sacar(1001);
@@ -53,7 +55,7 @@ public class MonederoTest {
   }
 
   @Test
-  public void ExtraerMasDe1000() {
+  public void ExtraerMasDe1000TiraExcepcion() {
     assertThrows(MaximoExtraccionDiarioException.class, () -> {
       cuenta.setSaldo(5000);
       cuenta.sacar(1001);
@@ -61,7 +63,7 @@ public class MonederoTest {
   }
 
   @Test
-  public void ExtraerMontoNegativo() {
+  public void ExtraerMontoNegativoTiraExcepcion() {
     assertThrows(MontoNegativoException.class, () -> cuenta.sacar(-500));
   }
 
